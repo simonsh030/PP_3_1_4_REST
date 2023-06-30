bbc257db230dc5920a2031f09d226ea38c1ed0ae
simonsh030 <icesert75@gmail.com>
2023-06-30 08:11:32 +0300

    Update Role.java

--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -11,6 +11,7 @@ import java.util.Set;
 public class Role implements GrantedAuthority {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
